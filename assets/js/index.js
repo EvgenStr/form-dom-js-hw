923299df363fe26695db4ac3a99f44dbc6514541
EvgenStr <juve2910@gmail.com>
2021-04-27 13:02:49 +0300

    push repo

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,11 @@ const arr = [];
 form.addEventListener('submit', (event) => {
   event.preventDefault()
   if (!event.target.reportValidity()) return;
-  const { target, target: { elements: { test: { value } } } } = event;
+  const { target, target:
+    { elements: {
+      test: { value }
+    }
+    } } = event;
   arr.push(value);
   target.reset();
   const li = document.createElement('li');
@@ -14,16 +18,11 @@ form.addEventListener('submit', (event) => {
   const ul = document.getElementById('list');
   span.innerText = value;
   del.innerText = "delete";
-  del.addEventListener('click', ({target}) => {
-    // arr.splice(arr.indexOf(e.target.parentNode.childNodes[0].data), 1);
-    arr.splice(arr.indexOf(target.parentNode.childNodes[0].innerText), 1);
-    // console.dir(e.target.parentNode.childNodes[0].data)
-    // console.dir(e.target.parentNode.childNodes[0].innerText)
-
-    target.parentNode.remove()
-  })
+  del.addEventListener('click', deleteParent)
   li.append(span, del);
-
   ul.append(li);
-
 })
+function deleteParent({ target: { parentNode } }) {
+  arr.splice(arr.indexOf(parentNode.childNodes[0].innerText), 1);
+  parentNode.remove();
+};
