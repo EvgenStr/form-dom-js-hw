ac27958fd6fc2463dc80d7c1bc1a0770ec1242bf
EvgenStr <juve2910@gmail.com>
2021-04-27 12:40:31 +0300

    add new styles

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,21 +4,26 @@ const arr = [];
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
-  console.log( event.target.reportValidity())
+  if (!event.target.reportValidity()) return;
   const { target, target: { elements: { test: { value } } } } = event;
   arr.push(value);
   target.reset();
   const li = document.createElement('li');
-  li.innerText = value;
   const del = document.createElement('button');
-  del.innerText = "delete";
-  del.addEventListener('click', (e) => {
-    arr.splice(arr.indexOf(e.target.parentNode.childNodes[0].data), 1);
-    console.dir(e.target.parentNode.childNodes[0].data)
-    e.target.parentNode.remove()
-  })
-  li.append(del);
+  const span = document.createElement('span');
   const ul = document.getElementById('list');
+  span.innerText = value;
+  del.innerText = "delete";
+  del.addEventListener('click', ({target}) => {
+    // arr.splice(arr.indexOf(e.target.parentNode.childNodes[0].data), 1);
+    arr.splice(arr.indexOf(target.parentNode.childNodes[0].innerText), 1);
+    // console.dir(e.target.parentNode.childNodes[0].data)
+    // console.dir(e.target.parentNode.childNodes[0].innerText)
+
+    target.parentNode.remove()
+  })
+  li.append(span, del);
+
   ul.append(li);
-  
+
 })
